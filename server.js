45a0e9979c5f30e25dd776567be70446b0aa7073
kana03 <camille.naulet@hotmail.fr>
2022-11-10 17:17:11 +0100

    deploy

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const express = require('express');
 const middlewares = jsonServer.defaults();
 const router = jsonServer.router('db.json');
-const port = process.env.PORT || 3001;
 
-app.use('/user', middlewares, router);
+app.use(middlewares);
+app.use(router);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port);
+app.listen(process.env.PORT || 3001);
