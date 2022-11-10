de186a4b2d09efce4967aaa208bcfa99aeca8d13
kana03 <camille.naulet@hotmail.fr>
2022-11-10 15:16:44 +0100

    deploy

--- serverdb.js
+++ serverdb.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: "./build" });
 const port = 3000;
 server.use(middlewares);
 server.use(router);
