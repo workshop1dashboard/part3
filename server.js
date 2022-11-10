0fddeb1abdecb65d942efd1c7e8c2a816928cc2f
kana03 <camille.naulet@hotmail.fr>
2022-11-10 15:54:16 +0100

    deploy

--- server.js
+++ server.js
@@ -9,8 +9,4 @@ const port = process.env.PORT || 3001;
 app.use('/user', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.listen(port);
