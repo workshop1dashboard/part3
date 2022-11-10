aea3b968d88c9a4dbdace4be51cff62c76883d3a
kana03 <camille.naulet@hotmail.fr>
2022-11-10 17:06:13 +0100

    deploy

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const jsonServer = require('json-server');
-const app = jsonServer.create();
+// const app = jsonServer.create();
 const path = require('path');
 const express = require('express');
-const port = process.env.PORT || 3001;
-const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
-
-app.use(middlewares, router);
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-const server = express();
-server.use('/user', app);
-
-server.listen(port);
+const app = express();
+app.set('port', (process.env.PORT || 3001));
+app.use(jsonServer.defaults());
+app.use('/user', jsonServer.router('db.json'))
+// const middlewares = jsonServer.defaults();
+// const router = jsonServer.router('db.json');
+// const port = process.env.PORT || 3001;
+//
+// app.use('/user', middlewares, router);
+// app.use(express.static(path.join(__dirname, 'dist')));
+//
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+//
+// app.listen(port);
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'));
+    if (process.send)
+        process.send('online')
+})
