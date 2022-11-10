72b48eec020b35b4e7454f810263f1bdf35c86d9
kana03 <camille.naulet@hotmail.fr>
2022-11-10 14:45:09 +0100

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+server.use(router)
+const port = process.env.PORT || 3000
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
 app.listen(port);
